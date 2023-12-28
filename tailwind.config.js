0db8c4af2aa6eb22ddcf0d4961aa5d0e8b5a4f65
Žan Stavbar <zanstavbar057@gmail.com>
2023-12-28 16:44:08 +0100

    DasyUI, Some font's

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage : {
+        'productivity' : 'url("/assets/images/bgs/5803426.jpg")'
+      },
+      fontFamily: {
+        'Sevillana': ['"Sevillana"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "white", // name of one of the included themes for dark mode
+    base: false, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 }
 
